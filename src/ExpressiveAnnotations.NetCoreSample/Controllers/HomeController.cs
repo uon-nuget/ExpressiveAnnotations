925282cf9eaf281fd69f6f3234b121b33651f664
Mark Berry <mark.berry@nottingham.ac.uk>
2018-08-21 14:08:16 +0100

    Removed NetCoreSample project and replaced with vanilla .Net Core 2.1 web project.

--- src/ExpressiveAnnotations.NetCoreSample/Controllers/HomeController.cs
+++ src/ExpressiveAnnotations.NetCoreSample/Controllers/HomeController.cs
@@ -1,66 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Net.Http;
-using System.Net.Http.Formatting;
+using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Mvc;
-using ExpressiveAnnotations.MvcWebSample.Models;
-using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using ExpressiveAnnotations.NetCoreSample.Models;
 
-namespace ExpressiveAnnotations.MvcWebSample.Controllers
+namespace ExpressiveAnnotations.NetCoreSample.Controllers
 {
-    public class HomeController : BaseController
+    public class HomeController : Controller
     {
-        public ActionResult Index()
+        public IActionResult Index()
         {
-            var model = new Query
-            {
-                GoAbroad = true,
-                Country = "Poland",
-                NextCountry = "Other",
-                SportType = "Extreme",
-                AgreeForContact = false,
-                SelectedCurrencies = new List<bool> {false, false},
-                LatestSuggestedReturnDate = DateTime.Today.AddMonths(1)
-            };
-
-            Session["Postbacks"] = (int?) TempData["Postbacks"] ?? 0;
-            ViewBag.Success = TempData["Success"];
-            return View("Home", TempData["Query"] as Query ?? model);
+            return View();
         }
 
-        [HttpPost]
-        public async Task<ActionResult> Index(Query model)
+        public IActionResult About()
         {
-            Session["Postbacks"] = (int) Session["Postbacks"] + 1;
-            if (ModelState.IsValid)
-            {
-                var result = await Save(model);
-                if (!result.IsSuccessStatusCode)
-                    throw new ApplicationException("Unexpected failure in WebAPI pipeline.");
+            ViewData["Message"] = "Your application description page.";
 
-                TempData["Postbacks"] = Session["Postbacks"];
-                TempData["Success"] = "[query successfully submitted]";
-                TempData["Query"] = model;
-                return RedirectToAction("Index"); // PRG to avoid subsequent form submission attempts on page refresh (http://en.wikipedia.org/wiki/Post/Redirect/Get)
-            }
-
-            return View("Home", model);
+            return View();
         }
 
-        private async Task<HttpResponseMessage> Save(Query model) // simulate saving through WebAPI call
+        public IActionResult Contact()
         {
-            using (var client = new HttpClient())
-            {
-                Debug.Assert(Request.Url != null);
-                client.BaseAddress = new Uri($"{Request.Url.Scheme}://{Request.Url.Authority}/");
-                var formatter = new JsonMediaTypeFormatter
-                {
-                    SerializerSettings = {ReferenceLoopHandling = ReferenceLoopHandling.Ignore}
-                };
-                return await client.PostAsync("api/Default/Save", model, formatter);
-            }
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
