3ee8975931e27e76e399c180704825988acb6404
Mark Berry <mark.berry@nottingham.ac.uk>
2018-08-21 14:36:13 +0100

    Removed some bits from the NetCoreSample home controller that we're not interested in here.

--- src/ExpressiveAnnotations.NetCoreSample/Controllers/HomeController.cs
+++ src/ExpressiveAnnotations.NetCoreSample/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ExpressiveAnnotations.NetCoreSample.Models;
 
@@ -15,24 +11,7 @@ namespace ExpressiveAnnotations.NetCoreSample.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
 
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
