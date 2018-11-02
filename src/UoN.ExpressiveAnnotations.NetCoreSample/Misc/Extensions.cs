876c797329a0a325ea6917846d69dc96a12c63c2
Mark Berry <mark.berry@nottingham.ac.uk>
2018-11-02 13:16:21 +0000

    Removed unused directives throughout solution.

--- src/UoN.ExpressiveAnnotations.NetCoreSample/Misc/Extensions.cs
+++ src/UoN.ExpressiveAnnotations.NetCoreSample/Misc/Extensions.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using Microsoft.AspNetCore.Html;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
--- src/UoN.ExpressiveAnnotations.NetCoreSample/Misc/VerbosityManager.cs
+++ src/UoN.ExpressiveAnnotations.NetCoreSample/Misc/VerbosityManager.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net;
 using Microsoft.AspNetCore.Http;
 
 namespace UoN.ExpressiveAnnotations.NetCoreSample.Misc
