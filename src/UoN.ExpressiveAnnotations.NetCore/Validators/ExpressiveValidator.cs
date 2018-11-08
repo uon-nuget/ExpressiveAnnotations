97c5c8444754333352955ac864f082b3e1628b00
Mark Berry <mark.berry@nottingham.ac.uk>
2018-11-08 11:58:57 +0000

    Removed call to reset suffix allocation in ExpressiveValidator base class constructor. This reset was relevant in the Framework version of EA but the Core version constructs client-side validation differently and needs to retain all of the session cache data throughout the session in order to recognise multiple annotations of the same time on a given property and assign them unique IDs.

--- src/UoN.ExpressiveAnnotations.NetCore/Validators/ExpressiveValidator.cs
+++ src/UoN.ExpressiveAnnotations.NetCore/Validators/ExpressiveValidator.cs
@@ -40,8 +40,6 @@ namespace UoN.ExpressiveAnnotations.NetCore.Validators
                 AttributeWeakId = $"{typeof(T).FullName}.{fieldId}".ToLowerInvariant();
                 FieldName = metadata.PropertyName;
 
-                ResetSuffixAllocation();
-
                 var item = ProcessStorage<string, CacheItem>.GetOrAdd(AttributeFullId, _ => // map cache is based on static dictionary, set-up once for entire application instance
                 {                                                                           // (by design, no reason to recompile once compiled expressions)
                     Debug.WriteLine($"[cache add] process: {Process.GetCurrentProcess().Id}, thread: {Thread.CurrentThread.ManagedThreadId}");
