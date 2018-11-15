c13410d95072022335a6fe7888a45d4637a3abde
Mark Berry <mark.berry@nottingham.ac.uk>
2018-11-15 10:57:17 +0000

    Removed unused directives.

--- src/UoN.ExpressiveAnnotations.NetCoreSample/Startup.cs
+++ src/UoN.ExpressiveAnnotations.NetCoreSample/Startup.cs
@@ -1,10 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using UoN.ExpressiveAnnotations.NetCore.Caching;
 using UoN.ExpressiveAnnotations.NetCore.DependencyInjection;
 using UoN.ExpressiveAnnotations.NetCoreSample.Misc;
 
